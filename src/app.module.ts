93495fe5a49e45c829349a7f061633f09c138608
donghoonlee <94donghoon.lee@gmail.com>
2025-11-15 16:02:41 +0900

    feat: inheritance

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
 import { StudentModel, TeacherModel } from './entity/person.entity';
+import {
+  AirplaneModel,
+  BookModel,
+  CarModel,
+  ComputerModel,
+  SingleBaseModel,
+} from './entity/inheritance.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserModel, StudentModel, TeacherModel]),
+    TypeOrmModule.forFeature([UserModel]),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -15,7 +22,16 @@ import { StudentModel, TeacherModel } from './entity/person.entity';
       username: 'postgres',
       password: 'postgres',
       database: 'typeormstudy',
-      entities: [UserModel, StudentModel, TeacherModel],
+      entities: [
+        UserModel,
+        StudentModel,
+        TeacherModel,
+        BookModel,
+        CarModel,
+        SingleBaseModel,
+        ComputerModel,
+        AirplaneModel,
+      ],
       synchronize: true,
     }),
   ],
